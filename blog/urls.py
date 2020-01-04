5c2b4ce45b27672530283af2ba08bd7668ee23f9
Jiwoo Jeon <jjw2995@gmail.com>
2020-01-04 10:41:10 -0800

    tu10 - CRUD via classView

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from . import views
+
+urlpatterns = [
+
+    # class based view .as_view()
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    path('post/new/', PostCreateView.as_view(), name='post-create'), #post_form.html
+
+    path('about/', views.about, name='blog-about')
+]
+
